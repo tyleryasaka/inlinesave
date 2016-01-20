e1ad030c7d1a2b946a9717b00983a9313c288356
Steve James <4stevejames@gmail.com>
2016-01-20 08:04:20 -0800

    Update plugin.js
    Added noop function for convenience and editor references to onSuccess and onFailure call backs to allow easily working with the current editor instance in those call backs.

--- inlinesave/plugin.js
+++ inlinesave/plugin.js
@@ -6,52 +6,52 @@ CKEDITOR.plugins.add( 'inlinesave',
 			{
 				exec : function( editor )
 				{
-					(function addData() {
-						var data = editor.getData(),
-						    dataID = editor.container.getId(),
-						    onSuccess = function() {},  	 	// callback when save is successful
-						    onFailure = function(statusCode) {}, 	// callback when save fails
-						    inlinesavecfg = editor.config.inlinesave,
-						    url = inlinesavecfg.postUrl || ''; 		// url to post data to
-						onSuccess = inlinesavecfg.onSuccess || onSuccess;
-						onFailure = inlinsavecfg.onFailure || onFailure;
-						if (!!inlinesavecfg.useJqueryPost) { 		// use the old jquery post method if desired
-							jQuery.ajax({
-								type: "POST",
-								url: url,
-								data: {
-									editabledata: data,
-									editorID: dataID
-								},
-								success: function(data, textStatus, jqXHR) {
-									onSuccess(data);   	// Allow server to return data
-								},
-								error: function (jqXHR, textStatus, errorThrown) {
-									onFailure(jqXHR.status);
-								}
-							});
-						}
-						else { // use pure javascript and send the data in json format (default)
-							var payload = JSON.stringify({
+					var data = editor.getData(),
+					    dataID = editor.container.getId(),
+					    inlinesavecfg = editor.config.inlinesave,
+					    url = inlinesavecfg.postUrl || '', 			// url to post data to
+					    noop = function() {},
+					    onSuccess = (typeof inlinesavecfg.onSuccess === "function") 
+							? inlinesavecfg.onSuccess || noop, 	// callback when save is successful
+					    onFailure = (typeof inlinesavecfg.onFailure === "function") 
+							? inlinesavecfg.onFailure || noop; 	// callback when save fails
+					    
+					if (!!inlinesavecfg.useJqueryPost) { 			// use the old jquery post method if desired
+						jQuery.ajax({
+							type: "POST",
+							url: url,
+							data: {
 								editabledata: data,
 								editorID: dataID
-							});
-							var xhttp = new XMLHttpRequest();
-							xhttp.onreadystatechange = function() {
-								if(xhttp.readyState == 4) {
-									if(xhttp.status == 200) {
-										onSuccess(xhttp.response); // Allow server to return data
-									}
-									else {
-										onFailure(xhttp.status);
-									}
+							},
+							success: function(data, textStatus, jqXHR) {
+								onSuccess(data, editor);   	// Allow server to return data
+							},
+							error: function (jqXHR, textStatus, errorThrown) {
+								onFailure(jqXHR.status, editor);
+							}
+						});
+					}
+					else { // use pure javascript and send the data in json format (default)
+						var payload = JSON.stringify({
+							editabledata: data,
+							editorID: dataID
+						});
+						var xhttp = new XMLHttpRequest();
+						xhttp.onreadystatechange = function() {
+							if(xhttp.readyState == 4) {
+								if(xhttp.status == 200) {
+									onSuccess(xhttp.response, editor); // Allow server to return data
 								}
-							};
-							xhttp.open("POST", url, true);
-							xhttp.setRequestHeader("Content-type", 'application/json');
-							xhttp.send(payload);
-						}
-					}());
+								else {
+									onFailure(xhttp.status, editor);
+								}
+							}
+						};
+						xhttp.open("POST", url, true);
+						xhttp.setRequestHeader("Content-type", 'application/json');
+						xhttp.send(payload);
+					}
 				}
 			});
 		editor.ui.addButton( 'Inlinesave',
