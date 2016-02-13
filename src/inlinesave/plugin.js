4503cedbe4cac462d3de8664f75b462bc5701119
Marc <marc-gist@users.noreply.github.com>
2016-02-13 10:51:06 -0500

    onSave return true/false to control send data
    onSave now needs to return true/false which will control if the data is sent to the server (save happens or not).
    if no onSave set, then default is to always try to send.

--- src/inlinesave/plugin.js
+++ src/inlinesave/plugin.js
@@ -4,14 +4,16 @@ CKEDITOR.plugins.add( 'inlinesave',
 	{
 		var config = editor.config.inlinesave,
 		    iconName = !!config.useColorIcon ? 'inlinesave-color.svg' : 'inlinesave.svg';
+
 		editor.addCommand( 'inlinesave',
 			{
 				exec : function( editor )
 				{
+                    var sendDataOk = true;
 					var postData = {},
 					    payload = '',
 					    contentType = 'application/x-www-form-urlencoded; charset=UTF-8';
-					
+
 					if (typeof config == "undefined") { // Give useful error message if user doesn't define config.inlinesave
 						throw new Error("CKEditor inlinesave: You must define config.inlinesave in your configuration file. See http://ckeditor.com/addon/inlinesave");
 						return;
@@ -22,48 +24,54 @@ CKEDITOR.plugins.add( 'inlinesave',
 					}
 
 					if (typeof config.onSave == "function") {
-						config.onSave(editor); // Allow showing 'loading' spinner
+						sendDataOk = config.onSave(editor); // Allow showing 'loading' spinner
 					}
-					
-					// Clone postData object from config and add editabledata and editorID properties
-					CKEDITOR.tools.extend(postData, config.postData || {}, true); // Clone config.postData to prevent changing the config.
-					postData.editabledata = editor.getData();
-					postData.editorID = editor.container.getId();
-					
-					// If user opts to use JSON format for sending data to server, use Content-type 'application/json'.
-					if(!!config.useJSON) {
-						payload = JSON.stringify(postData);
-						contentType = 'application/json; charset=UTF-8';
-					}
-					// Otherwise use the default Content-type, 'application/x-www-form-urlencoded'.
-					else {
-						// Convert postData object to multi-part form data query string for post like jQuery does by default.
-						var formData = '';
-						for (var key in postData) { // Must encode data to handle special characters
-							formData += '&' + key + '=' + encodeURIComponent(postData[key]);  
-						}
-						payload = formData.slice(1); // Remove initial '&'
-					}
-					
-					// Use pure javascript (no dependencies) and send the data in json format...
-					var xhttp = new XMLHttpRequest();
-					xhttp.onreadystatechange = function() {
-						if (xhttp.readyState == 4) {
-							// If success, call onSuccess callback if defined
-							if (typeof config.onSuccess == "function" && xhttp.status == 200) {
-								// Allow server to return data via xhttp.response
-								config.onSuccess(editor, xhttp.response); 
-							}
-							// If error, call onFailure callback if defined
-							else if (typeof config.onFailure == "function") {
-								config.onFailure(editor, xhttp.status, xhttp);
-							}
-						}
-					};
-					xhttp.open("POST", config.postUrl, true);
-					// Send as form data encoded to handle special characters.
-					xhttp.setRequestHeader("Content-type", contentType);
-					xhttp.send(payload);
+
+                    if (!sendDataOk) {
+                        throw new Error("CKEditor inlinesave: Saving Disable by return of onSave function = false");
+                        return;
+                    } else {
+
+                        // Clone postData object from config and add editabledata and editorID properties
+                        CKEDITOR.tools.extend(postData, config.postData || {}, true); // Clone config.postData to prevent changing the config.
+                        postData.editabledata = editor.getData();
+                        postData.editorID = editor.container.getId();
+
+                        // If user opts to use JSON format for sending data to server, use Content-type 'application/json'.
+                        if (!!config.useJSON) {
+                            payload = JSON.stringify(postData);
+                            contentType = 'application/json; charset=UTF-8';
+                        }
+                        // Otherwise use the default Content-type, 'application/x-www-form-urlencoded'.
+                        else {
+                            // Convert postData object to multi-part form data query string for post like jQuery does by default.
+                            var formData = '';
+                            for (var key in postData) { // Must encode data to handle special characters
+                                formData += '&' + key + '=' + encodeURIComponent(postData[key]);
+                            }
+                            payload = formData.slice(1); // Remove initial '&'
+                        }
+
+                        // Use pure javascript (no dependencies) and send the data in json format...
+                        var xhttp = new XMLHttpRequest();
+                        xhttp.onreadystatechange = function () {
+                            if (xhttp.readyState == 4) {
+                                // If success, call onSuccess callback if defined
+                                if (typeof config.onSuccess == "function" && xhttp.status == 200) {
+                                    // Allow server to return data via xhttp.response
+                                    config.onSuccess(editor, xhttp.response);
+                                }
+                                // If error, call onFailure callback if defined
+                                else if (typeof config.onFailure == "function") {
+                                    config.onFailure(editor, xhttp.status, xhttp);
+                                }
+                            }
+                        };
+                        xhttp.open("POST", config.postUrl, true);
+                        // Send as form data encoded to handle special characters.
+                        xhttp.setRequestHeader("Content-type", contentType);
+                        xhttp.send(payload);
+                    }
 				}
 			});
 		editor.ui.addButton( 'Inlinesave',
