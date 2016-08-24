8df84c8686601d9003eb34bb0813713b1c46bc59
Tyler Yasaka <tyler@partcycle.com>
2016-08-23 22:50:15 -0500

    Throw error if config is not defined.

--- src/inlinecancel/plugin.js
+++ src/inlinecancel/plugin.js
@@ -3,7 +3,15 @@ CKEDITOR.plugins.add( 'inlinecancel',
 	init: function( editor )
 	{
 		var config = editor.config.inlinecancel,
-		    iconName = !!config.useColorIcon ? 'inlinecancel-color.svg' : 'inlinecancel.svg';
+			iconName;
+
+		if (typeof config == "undefined") { // Give useful error message if user doesn't define config.inlinesave
+			throw new Error("CKEditor inlinecancel: You must define config.inlinecancel in your configuration file. See http://ckeditor.com/addon/inlinecancel");
+			return;
+		}
+
+		iconName = !!config.useColorIcon ? 'inlinecancel-color.svg' : 'inlinecancel.svg';
+
 		editor.addCommand( 'inlinecancel',
 			{
 				exec : function( editor )
