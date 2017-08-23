aee62601aa18cc6cbfb6f74f33b248901a286166
Simon Fetzel <simon.fetzel@get-the-solution.net>
2017-08-23 15:09:59 +0200

    corrected typo

--- src/inlinesave/lang/en.js
+++ src/inlinesave/lang/en.js
@@ -2,5 +2,5 @@
 CKEDITOR.plugins.setLang( 'inlinesave', 'en', {
 	successMessage: "Successfully saved",
         errorMessage: "Error while saving",
-        leaveConfirmMessage: "You have unsaved changed! Do you want to continue without saving?",
+        leaveConfirmMessage: "You have unsaved changes! Do you want to continue without saving?",
 } );
